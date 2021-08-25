cbef4f222b047675c0c1b6a071144498d80c061c
Alessandro Carinato <alessandro.carinato@gmail.com>
2021-08-25 12:38:12 +0200

    initial setup and frst intertive

---
+++ client/components/intertives/RadioItem.js
@@ -0,0 +1,10 @@
+const RadioItem = ({option}) => {
+    return ( 
+        <label className='radio-container'>{option}
+            <input  type='radio' name='select-proprietá' value={option}/>
+            <span className='checkmark'></span>    
+        </label>
+     );
+}
+ 
+export default RadioItem;
---
+++ client/pages/intertives/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Intertives = () => {
+    return ( 
+        <div className='container'>
+            <Link href='/intertives/rendita-catastale'>
+                Rendita Catastale
+            </Link>
+        </div>
+     );
+}
+ 
+export default Intertives;
